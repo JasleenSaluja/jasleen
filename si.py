0b38478689635955d90304e31bec23085747c3f5
Jasleen Saluja <augjasleen29@gmail.com>
2023-01-30 15:36:08 +0530

    first commit

---
+++ si.py
@@ -0,0 +1,10 @@
+# si=p*r*t/100
+p=int(input('principal:'))
+r=int(input('rate:'))
+t=int(input('time:'))
+si=p*r*t/100
+print("simple interest calculate")
+print(f'{p=}')
+print(f'{r=}')
+print(f'{t=}')
+print(f'simple interest:{si}')
